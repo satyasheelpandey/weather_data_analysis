01df4f5e7b7d4ef3d01fc91597dfa347efa3bdb7
pande519 <satya.pandey@kpn.com>
2020-03-07 06:56:43 +0100

    adding the code to get the data from KNMI

--- prepare_data/retrieve_data.py
+++ prepare_data/retrieve_data.py
@@ -1,19 +1,17 @@
 import urllib.request
 
-# url = 'http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi'
+list_years = [1979, 1989, 1999, 2009, 2019]
 
+for year in list_years:
+    data = {"stns": "251:260:350", "TEMP": "SUNR:PRCP", "byear": year, "bmonth": "7",
+            "bday": "1", "eyear": year, "emonth": "7", "eday": "31"}
 
-data={"stns": "251:260:350", "TEMP": "SUNR:PRCP", "byear": "2019", "bmonth": "7",
-                                   "bday": "1", "eyear": "2019", "emonth": "7", "eday": "31"}
+    params = urllib.parse.urlencode(data).encode("utf-8")
+    req = urllib.request.Request(url='http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi')
 
-params = urllib.parse.urlencode(data).encode("utf-8")
-#     'stns=251:260:350&TEMP:SUNR:PRCP&byear=2019&bmonth=7&bday=1&eyear=2019&emonth=7&eday=31')
-#
-#
-# f = urllib.request.urlopen('http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi')
-
-req = urllib.request.Request(url='http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi')
-# # #
-with urllib.request.urlopen(req,data=params) as f:
-    resp = f.read()
-    print(resp)
+    with urllib.request.urlopen(req, data=params) as response:
+        resp = response.read()
+        # print(resp)
+        f = open("../weather_files/weather_" + str(year) + ".csv", "w")
+        f.write(resp.decode("utf-8"))
+        f.close()
